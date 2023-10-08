598e381a7b4b1133be791775bde3f83c6e0aefad
Zemorath <trentward10@gmail.com>
2023-10-08 14:23:57 -0500

    Search page styling added

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+
 function SearchPage() {
-    console.log("hi")
+    return (
+        <div class="ui middle aligned center aligned grid">
+            <div class="column">
+                <h2 class="ui black header">
+                    <div class="content">Add a book to your library</div>
+                </h2>
+                <form class="ui medium form">
+                    <div class="ui stacked segment">
+                        <div class="field"> <h3>TITLE</h3>
+                            <div class="ui left text input">
+                                <input type="text" name="title" placeholder="Title" />
+                            </div>
+                        </div>
+                        <div class="field"> <h3>AUTHOR</h3>
+                            <div class="ui left text input">
+                                <input type="text" name="author" placeholder="Author" />
+                            </div>
+                        </div>
+                        <div class="field"> <h3>ISBN</h3>
+                            <div class="ui left text input">
+                                <input type="text" name="isbn" placeholder="ISBN" />
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default SearchPage;
