900808eedc5dc13276b7457deb6ca7630be8c23e
Zemorath <trentward10@gmail.com>
2023-10-08 15:42:10 -0500

    Beginnig searching for books to add

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,18 @@ function Header() {
     const [ isActive, setActive ] = useState(false)
 
     return (
-        <div class="ui container">
-            <h1 class="ui centered header">Z  E  E  R</h1>
-            <div class="ui large secondary inverted pointing menu">
+        <div className="ui container">
+            <h1 className="ui centered header">Z  E  E  R</h1>
+            <div className="ui large secondary inverted pointing menu">
                 <NavLink to="/" className={({ isActive }) => (isActive ? 'active' : 'item')}>Library</NavLink>
                 <NavLink to="/search" className={({ isActive }) => (isActive ? 'active' : 'item')}>Search</NavLink>
-                <div class="ui search box">
-                    <a class="ui centered input">
+                <div className="ui search box">
+                    <a className="ui centered input">
                         <input type="text" placeholder="Search Library..." />
                     </a>
                 </div>
-                <div class="right item">
-                    <a class="item">Sort By:</a>
+                <div className="right item">
+                    <a className="item">Sort By:</a>
                 </div>
             </div>
         </div>
--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 function SearchPage() {
+
+    const [ titleInput, setSearchTitle ] = useState('')
+    const [ authorInput, setSearchAuthor ] = useState('')
+    const [ isbnInput, setSearchIsbn ] = useState('')
+    const [ searchedBooks, setBooks ] = useState([])
+
+    function handleSearch() {
+
+        let search = () => {
+            if (isbnInput !== '') {
+                return isbnInput
+            } else if ( titleInput !== '') {
+                return titleInput
+            } else {
+                return authorInput
+            }
+        }
+        fetch(`https://openlibrary.org/search.json?q=${search}`)
+            .then((r) => r.json())
+            .then((searchedBooks) => console.log(searchedBooks))
+    }
+
+    // console.log(searchedBooks)
+
     return (
-        <div class="ui middle aligned center aligned grid">
-            <div class="column">
-                <h2 class="ui black header">
-                    <div class="content">Add a book to your library</div>
+        <div className="ui middle aligned center aligned grid">
+            <div className="column">
+                <h2 className="ui black header">
+                    <div className="content">Add a book to your library</div>
                 </h2>
-                <form class="ui medium form">
-                    <div class="ui stacked segment">
-                        <div class="field"> <h3>TITLE</h3>
-                            <div class="ui left text input">
-                                <input type="text" name="title" placeholder="Title" />
+                <form className="ui medium form" id="search" onSubmit={handleSearch}>
+                    <div className="ui stacked segment">
+                        <div className="field"> <h3>TITLE</h3>
+                            <div className="ui left text input">
+                                <input type="text" name="title" placeholder="Title" onChange={(e) => setSearchTitle(e.target.value)}/>
                             </div>
                         </div>
-                        <div class="field"> <h3>AUTHOR</h3>
-                            <div class="ui left text input">
-                                <input type="text" name="author" placeholder="Author" />
+                        <div className="field"> <h3>AUTHOR</h3>
+                            <div className="ui left text input">
+                                <input type="text" name="author" placeholder="Author" onChange={(e) => setSearchAuthor(e.target.value)}/>
                             </div>
                         </div>
-                        <div class="field"> <h3>ISBN</h3>
-                            <div class="ui left text input">
-                                <input type="text" name="isbn" placeholder="ISBN" />
+                        <div className="field"> <h3>ISBN</h3>
+                            <div className="ui left text input">
+                                <input type="text" name="isbn" placeholder="ISBN" onChange={(e) => setSearchIsbn(e.target.value)}/>
                             </div>
                         </div>
+                        <button className="ui fluid large black submit button" type="submit" >Search</button>
                     </div>
                 </form>
             </div>
