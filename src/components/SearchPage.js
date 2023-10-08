8b090e42cb09451c2e30840c490f3c6093dfe1a7
Zemorath <trentward10@gmail.com>
2023-10-08 16:22:55 -0500

    Fetch for search working properly.  Working to add cards for search results next.

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -8,9 +8,11 @@ function SearchPage() {
     const [ isbnInput, setSearchIsbn ] = useState('')
     const [ searchedBooks, setBooks ] = useState([])
 
-    function handleSearch() {
+    const [ isActive, setActive ] = useState(true)
 
-        let search = () => {
+    function handleSearch(e) {
+        e.preventDefault()
+        let search = (() => {
             if (isbnInput !== '') {
                 return isbnInput
             } else if ( titleInput !== '') {
@@ -18,41 +20,45 @@ function SearchPage() {
             } else {
                 return authorInput
             }
-        }
+        })();
+
         fetch(`https://openlibrary.org/search.json?q=${search}`)
             .then((r) => r.json())
             .then((searchedBooks) => console.log(searchedBooks))
+            .then(setActive(!isActive))
     }
 
-    // console.log(searchedBooks)
+    
 
     return (
         <div className="ui middle aligned center aligned grid">
-            <div className="column">
-                <h2 className="ui black header">
-                    <div className="content">Add a book to your library</div>
-                </h2>
-                <form className="ui medium form" id="search" onSubmit={handleSearch}>
-                    <div className="ui stacked segment">
-                        <div className="field"> <h3>TITLE</h3>
-                            <div className="ui left text input">
-                                <input type="text" name="title" placeholder="Title" onChange={(e) => setSearchTitle(e.target.value)}/>
+            {isActive && (
+                <div className="column">
+                    <h2 className="ui black header">
+                        <div className="content">Add a book to your library</div>
+                    </h2>
+                    <form className="ui medium form" id="search" onSubmit={handleSearch}>
+                        <div className="ui stacked segment">
+                            <div className="field"> <h3>TITLE</h3>
+                                <div className="ui left text input">
+                                    <input type="text" name="title" placeholder="Title" onChange={(e) => setSearchTitle(e.target.value)}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="field"> <h3>AUTHOR</h3>
-                            <div className="ui left text input">
-                                <input type="text" name="author" placeholder="Author" onChange={(e) => setSearchAuthor(e.target.value)}/>
+                            <div className="field"> <h3>AUTHOR</h3>
+                                <div className="ui left text input">
+                                    <input type="text" name="author" placeholder="Author" onChange={(e) => setSearchAuthor(e.target.value)}/>
+                                </div>
                             </div>
-                        </div>
-                        <div className="field"> <h3>ISBN</h3>
-                            <div className="ui left text input">
-                                <input type="text" name="isbn" placeholder="ISBN" onChange={(e) => setSearchIsbn(e.target.value)}/>
+                            <div className="field"> <h3>ISBN</h3>
+                                <div className="ui left text input">
+                                    <input type="text" name="isbn" placeholder="ISBN" onChange={(e) => setSearchIsbn(e.target.value)}/>
+                                </div>
                             </div>
+                            <button className="ui fluid large black submit button" type="submit" >Search</button>
                         </div>
-                        <button className="ui fluid large black submit button" type="submit" >Search</button>
-                    </div>
-                </form>
-            </div>
+                    </form>
+                </div>
+            )}    
         </div>
     )
 }
