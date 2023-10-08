6d7e0e6696e7bdc8d77cfc5c45274a7bb22d5e9f
Zemorath <trentward10@gmail.com>
2023-10-07 19:10:18 -0500

    Patch to add cover photo functionality added

--- src/components/BookInfo.js
+++ src/components/BookInfo.js
@@ -1,13 +1,50 @@
 import React, {useState, useEffect} from "react";
 
 function BookInfo({ book }) {
-    const { title, author, isbn } = book;
+    const { title, author, isbn, url } = book;
+
+    const [ showPic, setShowPic ] = useState(url)
+    const [ showButton, setShowButton ] = useState(() => {
+        if (url !== undefined) {
+            return false
+        } else {
+            return true
+        }
+    })
+
+    
+
+    function handlePicture() {
+        let url = prompt("Please enter photo URL.")
+        if (url !== null) {
+            let cover = new Image();
+            cover.src = url
+            
+            return fetch(`http://localhost:3000/books/${book.id}`, {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json"
+                },
+                body: JSON.stringify({
+                    "url": `${cover.src}`,
+                })
+            }).then(resp => resp.json())
+            .then(setShowPic(!showPic))
+            .then(setShowButton(!showButton))
+            
+        } else {
+
+        }
+    }
 
     return (
         
         <div class="card">
             <div class="image">
-                <></>
+                {showPic && (
+                    <img src={url} alt={title} />
+                )}
             </div>
             <div class="content">
                 <div class="header">
@@ -19,6 +56,11 @@ function BookInfo({ book }) {
                 <div class="description">
                     {author}
                 </div>
+            </div>
+            <div class="exta content">
+                <span class="right floated">
+                    {showButton && <button onClick={handlePicture} class="book-cover"> Add photo!</button>}
+                </span>
             </div>
         </div>
     
