a610ab8c79e0478e1877dda940c86a88b5e3532e
Zemorath <trentward10@gmail.com>
2023-11-05 16:59:30 -0600

    Finished initial formatting of home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,10 +2,18 @@ import React from "react"
 
 function Home() {
     return (
-        <div class="ui inverted vertical masthead center aligned segment">
-            <div class="ui text container">
-                <h1 class="ui inverted header">Welcome to Zeer Library</h1> 
-                <h2>Your library, online.</h2>
+        <div class="pusher">
+            <div class="ui inverted vertical masthead center aligned segment">
+                <div class="ui text container">
+                    <h1 class="ui inverted header">Welcome to Zeer Library</h1> 
+                    <h2>Your library, online.</h2>
+                </div>
+            </div>
+            <div class="ui vertical stripe segment">
+                <div class="ui middle aligned text container">
+                    <h3 class="ui centered header">Helping you keep track of your books</h3>
+                    <p class="ui centered">Digitally tracking your books helps keep your reading list organized. Know which books you've already read, are still intending to read, or are just there for show.</p>
+                </div>
             </div>
         </div>
     )
